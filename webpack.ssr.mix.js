d220128505177bc193de3579e3a2aae653a819d3
Color-Kat <katik7979@gmail.com>
2022-05-20 20:33:06 +0300

    server side rendering support

---
+++ webpack.ssr.mix.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const mix = require('laravel-mix')
+const nodeExternals = require('webpack-node-externals');
+
+mix
+    .options({ manifest: false })
+    .ts('resources/js/ssr.js', 'public/js')
+    .react()
+    .alias({ '@': path.resolve('resources/js') })
+    .webpackConfig(require('./webpack.config'))
+    .webpackConfig({
+        target: 'node',
+        externals: [nodeExternals()],
+    });
