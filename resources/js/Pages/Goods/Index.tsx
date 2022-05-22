75334211f65b23a9da2281307b20197ba5240444
Color-Kat <katik7979@gmail.com>
2022-05-22 15:10:20 +0300

    show goods table on client

--- resources/js/Pages/Goods/Index.tsx
+++ resources/js/Pages/Goods/Index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import {usePage} from "@inertiajs/inertia-react";
+import {Page} from "@inertiajs/inertia";
+
+interface IPaginator {
+    total: number;
+    data: {
+        id: number;
+        name: string;
+        price: number;
+    }[];
+}
 
 const Index: React.FC = () => {
+    const goods = usePage<Page<{ goods: IPaginator }>>().props.goods;
+    console.log(goods)
+
     return (
         <div>
-            Goods123
+            {goods.total > 0
+                ? <div>
+                    <table className="table-auto">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Control</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {goods.data.map(good => {
+                            return (
+                                <tr>
+                                    <td>{good.id}</td>
+                                    <td>{good.name}</td>
+                                    <td>{good.price}</td>
+                                    <td>
+                                        <button className="bg-green-600">Create</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+                : <div>
+                    Товаров нема
+                </div>
+            }
         </div>
     );
 };
